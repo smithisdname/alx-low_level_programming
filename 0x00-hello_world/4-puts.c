96e6f277994c631f876a4589f4e534eba76632f0
folarin folayan <crooksmith6@gmail.com>
2022-12-08 01:28:47 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always  0 (success)
  *
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
